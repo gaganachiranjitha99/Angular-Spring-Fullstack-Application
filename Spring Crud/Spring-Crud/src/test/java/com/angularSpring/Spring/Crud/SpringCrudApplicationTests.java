21f8e22edadcdff36ef600196cdb21f2b2067fec
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2024-03-30 12:36:23 +0530

    initial spring

---
+++ Spring Crud/Spring-Crud/src/test/java/com/angularSpring/Spring/Crud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularSpring.Spring.Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
