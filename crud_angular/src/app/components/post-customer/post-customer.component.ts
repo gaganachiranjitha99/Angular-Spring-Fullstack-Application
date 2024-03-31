4b22ffd85d8fe6998e831ea875912841d164fae0
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2024-03-31 14:15:33 +0530

    post customer api

--- crud_angular/src/app/components/post-customer/post-customer.component.ts
+++ crud_angular/src/app/components/post-customer/post-customer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/service/customer.service';
 
 @Component({
   selector: 'app-post-customer',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-customer.component.css']
 })
 export class PostCustomerComponent {
+  postCustomerForm:FormGroup |any ;
+
+  constructor(private customerService: CustomerService,
+    private fb:FormBuilder){}
+                
+    ngOnInit(){
+      this.postCustomerForm=this.fb.group({
+        name:[null,[Validators.required]],
+        phone:[null,[Validators.required]],
+        email:[null,[Validators.required,Validators.email]]
+      })
+    }
+
+    postCustomer(){
+      
+      this.customerService.postCustomer(this.postCustomerForm.value).subscribe((res)=>{
+        console.log(res)
+      })
+    }
 
 }
