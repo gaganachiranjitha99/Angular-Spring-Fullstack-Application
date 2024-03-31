e1d76ddd5fc8ad2d650057db0f6628ac06cd1a25
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2024-03-31 09:21:14 +0530

    update

--- crud_angular/src/app/app.module.ts
+++ crud_angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCustomerComponent } from './components/post-customer/post-customer.component';
+import { GetAllCustomersComponent } from './components/get-all-customers/get-all-customers.component';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCustomerComponent,
+    GetAllCustomersComponent,
+    UpdateCustomerComponent
   ],
   imports: [
     BrowserModule,
